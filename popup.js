9128efee2e09dabf80ad24b83adb03d8582303e7
seanande <seanande@andrew.cmu.edu>
2017-11-26 22:13:07 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,22 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
+  
+  var s = d.createElement('site');
+  s.type = 'hidden';
+  s.name = 'url';
+  s.value = tab.url;
+  // if s.value is in SiteList
+  //    block/wait/redirect/etc until verify is pressed
+  //    make extension visible
+  
   var verifyButton = document.getElementById('Verify');
   verifyButton.addEventListener('click', function() {
 
     chrome.tabs.getSelected(null, function(tab) {
       d = document;
 
-      /* sample code from other site, use as guideline */
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    
-    });
+      var pin = d.getElementById('pin');
+      var pinNum = pin.value;
+      //if pinNum == userPin
+      //    unblock/redirect to site
+      //    hide extension
+      
+    });  
   }, false);
+  
+  var addButton = document.getElementById('add');
+  addButton.addEventListener('click', function() {
+      //make new html that prompts for url to add to list
+  }. false);
 }, false);
