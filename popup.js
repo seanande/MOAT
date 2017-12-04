6a34451f0df3d8f510b8a8967a14cc10f65b9699
seanande <seanande@andrew.cmu.edu>
2017-12-04 11:05:53 -0500

    add sites to protected list
    User can navigate to a site, open the extension, and click "Add Page" to add the current site to their list of protected sites. Works with the domain name, so one click will work for all pages of a given site.

--- popup.js
+++ popup.js
@@ -1,48 +1,86 @@
 var userPinHash = 1234; //eventually hash this number to make it secure
 var SiteList = [];
 var mode = "user"; //hidden, user, verify(change), update(change)
-
+var currURL;
+var currHost;
 document.addEventListener('DOMContentLoaded', function() {
-  
-  var url = window.location.href;
-  if (SiteList.indexOf(window.location.hostname) != -1) {
+
+  chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs) {
+    window.currURL = tabs[0].url;
+  });
+
+  /*adapted from Stack Overflow answer 
+  function extractRootDomain(url) {
+    var domain = extractHostname(url);
+    var splitArr = domain.split('.');
+    var arrLen = splitArr.length;
+
+    //extracting the root domain here
+    //if there is a subdomain 
+    if (arrLen > 2) {
+      domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
+      //check to see if it's using a Country Code Top Level Domain (ccTLD) (i.e. ".me.uk")
+      if (splitArr[arrLen - 1].length == 2 && splitArr[arrLen - 1].length == 2) {
+        //this is using a ccTLD
+        domain = splitArr[arrLen - 3] + '.' + domain;
+      }
+    }
+    return domain;
+  }*/
+
+  //window.currHost = extractRootDomain(window.currURL);
+  //alert(window.currHost);
+
+  if (SiteList.indexOf(window.currHost) != -1) {
     //block/wait/redirect/etc until verify is pressed
     window.location.href = "file:///C:/Users/seana/Documents/placeholder.pdf"
     window.mode = "user";
     //make extension visible
   }
-  
+
   var verifyButton = document.getElementById('verify');
   verifyButton.addEventListener('click', function() {
     //chrome.tabs.getSelected(null, function(tab) {
       d = document;
-
-      var pin = d.getElementById('pin');
+      
+      var pin = d.getElementById("pin");
       var pinNum = pin.value;
-      var label   = d.getElementById("label");
+      var header = d.getElementById("header");
       var message = d.getElementById("message");
-
+      var label   = d.getElementById("label");
+      
+      message.innerHTML = "mode: " + window.mode + " pin: " + window.userPinHash;
+      label.innerHTML = "pin = " + pinNum;
       if(window.mode == "user") {
         if (pinNum == window.userPinHash) {
-          //redirect to site
-          window.location.href = url;
-          window.mode = "hidden";
+          //redirect current tab to site
+          //window.location.href = url;
+          //window.mode = "hidden";
+          message.innerHTML = "Access granted";
+          label.innerHTML = " ";
           //hide extension
+          d.hide;
         }
         else {
-          message.innerHTML = "incorrect PIN"
+          label.innerHTML = "incorrect PIN";
         }
-      } else if (window.mode == "verify") {
+      }
+      else if (window.mode == "verify") {
         if (pinNum == window.userPinHash) {
           //let user enter new password
-          label.innerHTML = "enter new PIN";
-          window.mode = "change";
+          message.innerHTML = "Enter new PIN";
+          label.innerHTML = " ";
+          window.mode = "update";
         }
         else {
-          message.innerHTML = "incorrect pin";
+          label.innerHTML = "Incorrect PIN";
         }
-      } else if (window.mode == "update") {
+      } 
+      else if (window.mode == "update") {
         window.userPinHash = pinNum;
+        d.getElementById('add').visible = true;
+        message.innerHTML = "Enter your 4-digit PIN";
+        label.innerHTML = "PIN updated"
       }
     //});
   }, false);
@@ -50,14 +88,21 @@ document.addEventListener('DOMContentLoaded', function() {
   var addButton = document.getElementById('add');
   addButton.addEventListener('click', function() {
     //add the current page to the list
-    SiteList.push(window.location.hostname);
+    var url  = window.currURL;
+    var host = (new URL(url)).hostname;
+    alert(url + "  " + host);
+    SiteList.push(host);
+    message = document.getElementById('message');
+    message.innerHTML = "Added " + host + " to your protected sites."
   }, false);
 
   var changeButton = document.getElementById('change');
   changeButton.addEventListener('click', function() {
     //prompt user to change their pin
-    var label = document.getElementById("label");
-    label.innerHTML = "enter old PIN";
+    var header = document.getElementById("header");
+    header.innerHTML = "Enter old PIN";
+    document.getElementById("add").visible = false;
+    window.mode = "verify";
 
   }, false);
 
