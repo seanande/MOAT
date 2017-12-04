b48586f3acabd6db6c25abf2000358009d461bcf
seanande <seanande@andrew.cmu.edu>
2017-12-04 11:49:02 -0500

    formatting

--- popup.js
+++ popup.js
@@ -2,34 +2,14 @@ var userPinHash = 1234; //eventually hash this number to make it secure
 var SiteList = [];
 var mode = "user"; //hidden, user, verify(change), update(change)
 var currURL;
-var currHost;
 document.addEventListener('DOMContentLoaded', function() {
 
+  /*makes URL object with data of the active tab*/
   chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs) {
-    window.currURL = tabs[0].url;
+    var url = tabs[0].url;
+    window.currURL = new URL(url);
   });
 
-  /*adapted from Stack Overflow answer 
-  function extractRootDomain(url) {
-    var domain = extractHostname(url);
-    var splitArr = domain.split('.');
-    var arrLen = splitArr.length;
-
-    //extracting the root domain here
-    //if there is a subdomain 
-    if (arrLen > 2) {
-      domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
-      //check to see if it's using a Country Code Top Level Domain (ccTLD) (i.e. ".me.uk")
-      if (splitArr[arrLen - 1].length == 2 && splitArr[arrLen - 1].length == 2) {
-        //this is using a ccTLD
-        domain = splitArr[arrLen - 3] + '.' + domain;
-      }
-    }
-    return domain;
-  }*/
-
-  //window.currHost = extractRootDomain(window.currURL);
-  //alert(window.currHost);
 
   if (SiteList.indexOf(window.currHost) != -1) {
     //block/wait/redirect/etc until verify is pressed
@@ -48,9 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var header = d.getElementById("header");
       var message = d.getElementById("message");
       var label   = d.getElementById("label");
-      
-      message.innerHTML = "mode: " + window.mode + " pin: " + window.userPinHash;
-      label.innerHTML = "pin = " + pinNum;
+
       if(window.mode == "user") {
         if (pinNum == window.userPinHash) {
           //redirect current tab to site
@@ -58,22 +36,27 @@ document.addEventListener('DOMContentLoaded', function() {
           //window.mode = "hidden";
           message.innerHTML = "Access granted";
           label.innerHTML = " ";
+          input.value = "";
           //hide extension
           d.hide;
         }
         else {
           label.innerHTML = "incorrect PIN";
+          input.value = "";
         }
       }
       else if (window.mode == "verify") {
+        /* Verify old PIN so they can update PIN */
         if (pinNum == window.userPinHash) {
-          //let user enter new password
+          //let user enter new PIN
           message.innerHTML = "Enter new PIN";
           label.innerHTML = " ";
           window.mode = "update";
+          input.value = "";
         }
         else {
           label.innerHTML = "Incorrect PIN";
+          input.value = "";
         }
       } 
       else if (window.mode == "update") {
@@ -81,6 +64,7 @@ document.addEventListener('DOMContentLoaded', function() {
         d.getElementById('add').visible = true;
         message.innerHTML = "Enter your 4-digit PIN";
         label.innerHTML = "PIN updated"
+        input.value = "";
       }
     //});
   }, false);
@@ -90,7 +74,6 @@ document.addEventListener('DOMContentLoaded', function() {
     //add the current page to the list
     var url  = window.currURL;
     var host = (new URL(url)).hostname;
-    alert(url + "  " + host);
     SiteList.push(host);
     message = document.getElementById('message');
     message.innerHTML = "Added " + host + " to your protected sites."
@@ -98,12 +81,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var changeButton = document.getElementById('change');
   changeButton.addEventListener('click', function() {
-    //prompt user to change their pin
-    var header = document.getElementById("header");
-    header.innerHTML = "Enter old PIN";
+    //prompt user to change their PIN
+    var message = document.getElementById("message");
+    message.innerHTML = "Enter old PIN";
     document.getElementById("add").visible = false;
     window.mode = "verify";
-
+    var input = document.getElementById("pin");
+    input.value = "";
   }, false);
 
 }, false);
