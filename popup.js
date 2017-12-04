f34a329def7c3da01f5700359a553210ce10497e
seanande <seanande@andrew.cmu.edu>
2017-12-03 22:18:31 -0500

    verification, add page, and update PIN
    added logic to verify a page, add a page to the protected list, and update the user's PIN

--- popup.js
+++ popup.js
@@ -1,30 +1,64 @@
+var userPinHash = 1234; //eventually hash this number to make it secure
+var SiteList = [];
+var mode = "user"; //hidden, user, verify(change), update(change)
+
 document.addEventListener('DOMContentLoaded', function() {
   
-  var s = d.createElement('site');
-  s.type = 'hidden';
-  s.name = 'url';
-  s.value = tab.url;
-  // if s.value is in SiteList
-  //    block/wait/redirect/etc until verify is pressed
-  //    make extension visible
+  var url = window.location.href;
+  if (SiteList.indexOf(window.location.hostname) != -1) {
+    //block/wait/redirect/etc until verify is pressed
+    window.location.href = "file:///C:/Users/seana/Documents/placeholder.pdf"
+    window.mode = "user";
+    //make extension visible
+  }
   
-  var verifyButton = document.getElementById('Verify');
+  var verifyButton = document.getElementById('verify');
   verifyButton.addEventListener('click', function() {
-
-    chrome.tabs.getSelected(null, function(tab) {
+    //chrome.tabs.getSelected(null, function(tab) {
       d = document;
 
       var pin = d.getElementById('pin');
       var pinNum = pin.value;
-      //if pinNum == userPin
-      //    unblock/redirect to site
-      //    hide extension
-      
-    });  
+      var label   = d.getElementById("label");
+      var message = d.getElementById("message");
+
+      if(window.mode == "user") {
+        if (pinNum == window.userPinHash) {
+          //redirect to site
+          window.location.href = url;
+          window.mode = "hidden";
+          //hide extension
+        }
+        else {
+          message.innerHTML = "incorrect PIN"
+        }
+      } else if (window.mode == "verify") {
+        if (pinNum == window.userPinHash) {
+          //let user enter new password
+          label.innerHTML = "enter new PIN";
+          window.mode = "change";
+        }
+        else {
+          message.innerHTML = "incorrect pin";
+        }
+      } else if (window.mode == "update") {
+        window.userPinHash = pinNum;
+      }
+    //});
   }, false);
   
   var addButton = document.getElementById('add');
   addButton.addEventListener('click', function() {
-      //make new html that prompts for url to add to list
-  }. false);
+    //add the current page to the list
+    SiteList.push(window.location.hostname);
+  }, false);
+
+  var changeButton = document.getElementById('change');
+  changeButton.addEventListener('click', function() {
+    //prompt user to change their pin
+    var label = document.getElementById("label");
+    label.innerHTML = "enter old PIN";
+
+  }, false);
+
 }, false);
